9a894fd5e2af647f0416d2fcd8d0161cdb356ce4
[Taufiq Dhanial] <[taufiq.dhanil@gmail.com]>
2022-11-17 19:26:16 +0700

    now read from database

--- server/src/resolver/Book.resolver.ts
+++ server/src/resolver/Book.resolver.ts
@@ -1,11 +1,10 @@
 import { Query, Resolver } from "type-graphql";
 import { Book } from "../entity/Book.entity";
-import { BooksData } from "../sampleData/Book.data";
 
 @Resolver(Book)
 export class BookResolver {
   @Query(() => [Book])
-  books() {
-    return BooksData;
+  async books() {
+    return await Book.find();
   }
 }
--- server/src/resolver/User.resolver.ts
+++ server/src/resolver/User.resolver.ts
@@ -1,11 +1,10 @@
 import { Query, Resolver } from "type-graphql";
 import { User } from "../entity/User.Entity";
-import { UsersData } from "../sampleData/User.data";
 
 @Resolver(User)
 export class UserResolver {
   @Query(() => [User])
-  users() {
-    return UsersData;
+  async users() {
+    return await User.find();
   }
 }
